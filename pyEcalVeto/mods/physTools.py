507d0f54b38b4c9b866a3199f66f53baa46171b5
aechavez <57649078+aechavez@users.noreply.github.com>
2022-04-08 21:57:54 -0700

    Finish code refactor
    Finish double-checking code

--- pyEcalVeto/mods/physTools.py
+++ pyEcalVeto/mods/physTools.py
@@ -302,38 +302,38 @@ hcal_STRIP_SHIFT = 0
 # Miscellaneous constants
 ###################################
 
-# 68% containment radii/layer for different bins in momentum/angle
-radius68_thetalt10_plt500 = [4.045666158618167,  4.086393662224346,  4.359141107602775,  4.666549994726691,  5.8569181911416015,
-                             6.559716356124256,  8.686967529043072,  10.063482736354674, 13.053528344041274, 14.883496407943747,
-                             18.246694748611368, 19.939799900443724, 22.984795944506224, 25.14745829663406,  28.329169392203216,
-                             29.468032123356345, 34.03271241527079,  35.03747443690781,  38.50748727211848,  39.41576583301171,
-                             42.63622296033334,  45.41123601592071,  48.618139095742876, 48.11801717451056,  53.220539860213655,
-                             58.87753380915155,  66.31550881539764,  72.94685877928593,  85.95506228335348,  89.20607201266672,
-                             93.34370253818409,  96.59471226749734,  100.7323427930147,  103.98335252232795]
-radius68_thetalt10_pgt500 = [4.081926458777424,  4.099431732299409,  4.262428482867968,  4.362017581473145,  4.831341579961153,
-                             4.998346041276382,  6.2633736512415705, 6.588371889265881,  8.359969947444522,  9.015085558044309,
-                             11.262722588206483, 12.250305471269183, 15.00547660437276,  16.187264014640103, 19.573764900578503,
-                             20.68072032434797,  24.13797140783321,  25.62942209291236,  29.027596514735617, 30.215039667389316,
-                             33.929540248019585, 36.12911729771914,  39.184563500620946, 42.02062468386282,  46.972125628650204,
-                             47.78214816041894,  55.88428562462974,  59.15520134927332,  63.31816666637158,  66.58908239101515,
-                             70.75204770811342,  74.022963432757,    78.18592874985525,  81.45684447449884]
-radius68_theta10to20 = [4.0251896715647115, 4.071661598616328, 4.357690094817289,  4.760224640141712,  6.002480766325418,
-                        6.667318981016246,  8.652513285172342, 9.72379373302137,   12.479492693251478, 14.058548828317289,
-                        17.544872909347912, 19.43616066939176, 23.594162859513734, 25.197329065282954, 29.55995803074302,
-                        31.768946746958296, 35.79247330197688, 37.27810357669942,  41.657281051476545, 42.628141392692626,
-                        47.94208483539388,  49.9289473559796,  54.604030254423975, 53.958762417361655, 53.03339560920388,
-                        57.026277390001425, 62.10810455035879, 66.10098633115634,  71.1828134915137,   75.17569527231124,
-                        80.25752243266861,  84.25040421346615, 89.33223137382352,  93.32511315462106]
-radius68_thetagt20 = [4.0754238481177705, 4.193693485630508,  5.14209420056253,   6.114996249971468,  7.7376807326481645,
-                      8.551663213602291,  11.129110612057813, 13.106293737495639, 17.186617323282082, 19.970887612094604,
-                      25.04088272634407,  28.853696411302344, 34.72538105333071,  40.21218694947545,  46.07344239520299,
-                      50.074953583805346, 62.944045771758645, 61.145621459396814, 69.86940198299047,  74.82378572939959,
-                      89.4528387422834,   93.18228303096758,  92.51751129204555,  98.80228884380018,  111.17537347472128,
-                      120.89712563907408, 133.27021026999518, 142.99196243434795, 155.36504706526904, 165.08679922962185,
-                      177.45988386054293, 187.18163602489574, 199.55472065581682, 209.2764728201696]
+# Arrays holding 68% containment radii/layer for different bins in momentum/angle
+radius68_thetalt10_plt500 = np.array([4.045666158618167,  4.086393662224346,  4.359141107602775,  4.666549994726691,  5.8569181911416015,
+                                      6.559716356124256,  8.686967529043072,  10.063482736354674, 13.053528344041274, 14.883496407943747,
+                                      18.246694748611368, 19.939799900443724, 22.984795944506224, 25.14745829663406,  28.329169392203216,
+                                      29.468032123356345, 34.03271241527079,  35.03747443690781,  38.50748727211848,  39.41576583301171,
+                                      42.63622296033334,  45.41123601592071,  48.618139095742876, 48.11801717451056,  53.220539860213655,
+                                      58.87753380915155,  66.31550881539764,  72.94685877928593,  85.95506228335348,  89.20607201266672,
+                                      93.34370253818409,  96.59471226749734,  100.7323427930147,  103.98335252232795])
+radius68_thetalt10_pgt500 = np.array([4.081926458777424,  4.099431732299409,  4.262428482867968,  4.362017581473145,  4.831341579961153,
+                                      4.998346041276382,  6.2633736512415705, 6.588371889265881,  8.359969947444522,  9.015085558044309,
+                                      11.262722588206483, 12.250305471269183, 15.00547660437276,  16.187264014640103, 19.573764900578503,
+                                      20.68072032434797,  24.13797140783321,  25.62942209291236,  29.027596514735617, 30.215039667389316,
+                                      33.929540248019585, 36.12911729771914,  39.184563500620946, 42.02062468386282,  46.972125628650204,
+                                      47.78214816041894,  55.88428562462974,  59.15520134927332,  63.31816666637158,  66.58908239101515,
+                                      70.75204770811342,  74.022963432757,    78.18592874985525,  81.45684447449884])
+radius68_theta10to20 = np.array([4.0251896715647115, 4.071661598616328, 4.357690094817289,  4.760224640141712,  6.002480766325418,
+                                 6.667318981016246,  8.652513285172342, 9.72379373302137,   12.479492693251478, 14.058548828317289,
+                                 17.544872909347912, 19.43616066939176, 23.594162859513734, 25.197329065282954, 29.55995803074302,
+                                 31.768946746958296, 35.79247330197688, 37.27810357669942,  41.657281051476545, 42.628141392692626,
+                                 47.94208483539388,  49.9289473559796,  54.604030254423975, 53.958762417361655, 53.03339560920388,
+                                 57.026277390001425, 62.10810455035879, 66.10098633115634,  71.1828134915137,   75.17569527231124,
+                                 80.25752243266861,  84.25040421346615, 89.33223137382352,  93.32511315462106])
+radius68_thetagt20 = np.array([4.0754238481177705, 4.193693485630508,  5.14209420056253,   6.114996249971468,  7.7376807326481645,
+                               8.551663213602291,  11.129110612057813, 13.106293737495639, 17.186617323282082, 19.970887612094604,
+                               25.04088272634407,  28.853696411302344, 34.72538105333071,  40.21218694947545,  46.07344239520299,
+                               50.074953583805346, 62.944045771758645, 61.145621459396814, 69.86940198299047,  74.82378572939959,
+                               89.4528387422834,   93.18228303096758,  92.51751129204555,  98.80228884380018,  111.17537347472128,
+                               120.89712563907408, 133.27021026999518, 142.99196243434795, 155.36504706526904, 165.08679922962185,
+                               177.45988386054293, 187.18163602489574, 199.55472065581682, 209.2764728201696])
 
-# Endpoints for each longitudinal segment
-segment_ends = [0, 6, 17, 34]
+# Array holding endpoints for each longitudinal segment
+segment_ends = np.array([0, 6, 17, 34])
 nsegments = len(segment_ends) - 1
 
 # Number of containment regions
@@ -341,9 +341,7 @@ nregions = 5
 
 # Simple class for storing hit data
 class HitData:
-
     def __init__(self, position = None, layer = None):
-
         self.position = position
         self.layer = layer
 
@@ -352,12 +350,15 @@ class HitData:
 # Miscellaneous functions
 ###################################
 
+# Function to get the position of a hit as a numpy array
+def get_position(hit):
+    return np.array([hit.getXPos(), hit.getYPos(), hit.getZPos()])
+
 # Function to get the layerZ of a hit
 def get_layerZ(hit):
-
     return ecal_layerZs[get_ecal_layer(hit)]
 
-# Function to project a point along a vector to a plane parallel to the XY-plane
+# Function to linearly project a point along a vector to a plane parallel to the XY-plane
 def linear_projection(x, u, z):
 
     x1, y1, z1 = x
@@ -374,53 +375,47 @@ def linear_projection(x, u, z):
 
     return np.array([x2, y2, z2])
 
-# Function to get the intercepts of a projected point for a collection of planes
+# Function to get the intercepts for a point projected through a collection of planes
 def get_projected_intercepts(x, u, zs):
-
     return np.array([linear_projection(x, u, z)[0:2] for z in zs])
 
-# Function to normalize a numpy array
-def normalize(array):
+# Function to normalize a vector
+def normalize(u):
 
-    norm = np.linalg.norm(array)
-    if norm == 0: return array
+    norm = np.linalg.norm(u)
+    if norm == 0: return u
 
-    return array/norm
+    return u/norm
 
 # Function to calculate the Euclidean distance between two points
 def distance(x, y):
-
     return np.sqrt(np.sum((x - y)**2))
 
-# Function to calculate the distance between a point and a line defined by two points
+# Function to calculate the distance between a point a line
 def distance_point_to_line(x, y1, y2):
 
     norm = np.linalg.norm(y1 - y2)
-    if norm == 0: return distance(x, y1)
+    if norm == 0: return np.sqrt(np.sum((x - y1)**2))
 
-    return np.linalg.norm(np.cross(x - y1, x - y2))/norm
+    return np.linalg.norm(np.cross(x - y1, y1 - y2))/norm
 
 # Function to calculate the distance between two lines
 def distance_line_to_line(x1, x2, y1, y2):
 
-    xhat = normalize(x1 - x2)
-    yhat = normalize(y1 - y2)
-    cross = np.cross(xhat, yhat)
+    cross = np.cross(x1 - x2, y1 - y2)
     norm = np.linalg.norm(cross)
 
     if norm == 0:
 
-        xnorm = np.linalg.norm(xhat)
-        ynorm = np.linalg.norm(yhat)
+        xnorm = np.linalg.norm(x1 - x2)
+        ynorm = np.linalg.norm(y1 - y2)
 
-        if xnorm == 0 and ynorm == 0: return distance(x1, y1)
+        if xnorm == 0 and ynorm == 0: return np.sqrt(np.sum((x1 - y1)**2))
+        elif ynorm == 0: return np.linalg.norm(np.cross(x1 - x2, x1 - y1))/xnorm
 
-        elif xnorm == 0: return distance_point_to_line(x1, y1, y2)
-        elif ynorm == 0: return distance_point_to_line(y1, x1, x2)
+        return np.linalg.norm(np.cross(x1 - y1, y1 - y2))/ynorm
 
-        return np.linalg.norm(np.cross(xhat, x1 - y1))
-
-    return -np.dot(cross, x1 - y1)/norm
+    return abs(np.dot(cross, x1 - y1)/norm)
 
 # Function to get the angle between two vectors
 def get_angle(u, v, units = 'radians'):
@@ -429,46 +424,35 @@ def get_angle(u, v, units = 'radians'):
     vhat = normalize(v)
 
     if units == 'radians': return np.arccos(np.dot(uhat, vhat))
-    elif units == 'degrees': return (180./np.pi)*np.arccos(np.dot(uhat, vhat))
-
-# Function to get the position of a hit as a numpy array
-def get_position(hit):
-
-    return np.array([hit.getXPos(), hit.getYPos(), hit.getZPos()])
+    elif units == 'degrees': return (180/np.pi)*np.arccos(np.dot(uhat, vhat))
 
 
 ##############################
 # Hit ID information
 ##############################
 
-# Function to get layerID from ECal hit
+# Function to get the layer ID from a ECal hit
 def ecal_layer(hit):
-
     return (hit.getID() >> ecal_LAYER_SHIFT) & ecal_LAYER_MASK
 
-# Function to get moduleID from ECal hit
+# Function to get the module ID from a ECal hit
 def ecal_module(hit):
-
     return (hit.getID() >> ecal_MODULE_SHIFT) & ecal_MODULE_MASK
 
-# Function to get cellID from ECal hit
+# Function to get the cell ID from a ECal hit
 def ecal_cell(hit):
-
     return (hit.getID() >> ecal_CELL_SHIFT) & ecal_CELL_MASK
 
-# Function to get sectionID from HCal hit
+# Function to get the section ID from a HCal hit
 def hcal_section(hit):
-
     return (hit.getID() >> hcal_SECTION_SHIFT) & hcal_SECTION_MASK
 
-# Function to get layerID from HCal hit
+# Function to get the layer ID from a HCal hit
 def hcal_layer(hit):
-
     return (hit.getID() >> hcal_LAYER_SHIFT) & hcal_LAYER_MASK
 
-# Function to get stripID from HCal hit
+# Function to get the strip ID from a HCal hit
 def hcal_strip(hit):
-
     return (hit.getID() >> hcal_STRIP_SHIFT) & hcal_STRIP_MASK
 
 
@@ -480,7 +464,7 @@ def hcal_strip(hit):
 # furthest downstream target scoring plane
 def get_electron_target_sp_hit(target_sp_hits):
 
-    pmax = 0.
+    pmax = 0
     target_sp_hit = None
     for hit in target_sp_hits:
 
@@ -501,7 +485,7 @@ def get_electron_target_sp_hit(target_sp_hits):
 # at the front ECal scoring plane
 def get_electron_ecal_sp_hit(ecal_sp_hits):
 
-    pmax = 0.
+    pmax = 0
     ecal_sp_hit = None
     for hit in ecal_sp_hits:
 
@@ -520,15 +504,14 @@ def get_electron_ecal_sp_hit(ecal_sp_hits):
 
 # Function to infer the photonuclear photon's position and momentum
 # at the target scoring plane
-def infer_photon_information(target_sp_hit):
-
+def infer_photon_info(target_sp_hit):
     return target_sp_hit.getPosition(), np.array([0., 0., 4000.])\
                                         - np.array(target_sp_hit.getMomentum())
 
 # Function to get the photon scoring plane hit at the ECal scoring plane
 def get_photon_ecal_sp_hit(ecal_sp_hits):
 
-    pmax = 0.
+    pmax = 0
     ecal_sp_hit = None
     for hit in ecal_sp_hits:
 
